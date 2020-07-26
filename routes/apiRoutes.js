5ae5c47c8da824caf4b9ab5c154d6e4bdec7492f
Maksim Verkhoturov <sd32@protonmail.com>
2020-07-26 09:20:08 -0600

    setting api and html routes, handling logic behind deleting and creating notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,36 @@
-const tableData = require("../data/tableData");
-const waitListData = require("../data/waitlistData");
+const fs = require("fs");
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 module.exports = app => {
-    app.get("/api/notes", (req, res) => {
-        // Should read the `db.json` file and return all saved notes as JSON.
+    app.get("/api/notes", async (req, res) => {
+        const db = await readFileAsync("db/db.json", "utf8");
+        res.json(JSON.parse(db));
     });
 
-    app.post("/api/tables", (req, res) => {
-        const reservation = req.body;
+    app.post("/api/notes", async (req, res) => {
+        const newNote = req.body;
+        const db = JSON.parse(await readFileAsync("db/db.json", "utf8"));
+        newNote.id = db.length + 1;
+        db.push(newNote);
+        await writeFileAsync("db/db.json", JSON.stringify(db), "utf8");
+        console.log(JSON.stringify(newNote));
+        res.json(JSON.stringify(newNote));
+    })
 
-        if (tableData.length < 5) {
-            tableData.push(reservation);
-            res.json(true);
-        } else {
-            waitListData.push(reservation);
-            res.json(false);
+
+    app.delete("/api/notes/:id", async (req, res) => {
+        const id = req.params.id - 1;
+        const db = JSON.parse(await readFileAsync("db/db.json", "utf8"));
+        db.splice(id, 1);
+        console.log(db, '\n');
+        for (const i in db) {
+            db[i].id = Number(i) + 1;
         }
-    });
-
-
-    app.post("/api/notes", (req, res) => {
-        // Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-    });
-
-
-    app.delete("/api/notes/:id", (req, res) => {
-        // Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+        console.log(db, '\n');
+        await writeFileAsync("db/db.json", JSON.stringify(db), "utf8");
+        res.send(true);
     });
 };
--- server.js
+++ server.js
@@ -10,21 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
-
-// ================================================================================
-// ROUTES
-// Note: The routes are stored in an external module and required here
-// ================================================================================
-
-// The (app) at the end of each require threads the `app` object through to the routes, so they can use methods like app.get(), app.post(), etc
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-
-
-// ================================================================================
-// Listener
-// ================================================================================
-// Kicks the server off to listen for requests on the specified port number
 app.listen(PORT, () => console.log(`App listening 'http://localhost:${PORT}'`));
